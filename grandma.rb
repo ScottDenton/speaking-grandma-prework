6d2a179e7b78d88835d240f8373037f2356ce195
scottyd_14 <scottyd_14@hotmail.com>
2018-10-03 16:48:32 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,11 @@
 def speak_to_grandma(string)
-  if string.upcase && string !="I LOVE YOU GRANDMA!"
-    "NO, NOT SINCE 1938!"
-  elsif string == "I LOVE YOU GRANDMA!"
+  if string == "I LOVE YOU GRANDMA!"
       "I LOVE YOU TOO PUMPKIN!"
-   
-  elsif string != string.upcase
+      
+      elsif string != string.upcase
        "HUH?! SPEAK UP,SONNY!"
-      end
+  
+else 
+  "NO, NOT SINCE 1938!"
+end
 end
