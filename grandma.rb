9bbd91aeed01b7e7675aa66d956c6d8d71b2c4db
scottyd_14 <scottyd_14@hotmail.com>
2018-10-03 16:44:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(string)
   if string.upcase && string !="I LOVE YOU GRANDMA!"
-    "HUH?! SPEAK UP, SONNY!"
+    "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
       "I LOVE YOU TOO PUMPKIN!"
   else
